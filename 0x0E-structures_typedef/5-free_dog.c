329dbc32fd1a04c9ecdc9d3d5311fa22c55e45c0
meran <meran.ahmed.tr@gmail.com>
2023-05-22 16:05:30 -0700

    5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib>
+
+/**
+ * free_dog -  frees dogs
+ * @d: dog
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
