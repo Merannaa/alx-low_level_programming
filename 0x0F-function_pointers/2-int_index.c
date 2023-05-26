1b2b1c8b60699ff53cb5cc72b761bf787d7e68b7
meran <meran.ahmed.tr@gmail.com>
2023-05-26 03:50:27 -0700

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,14 +9,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int x = 0;
-	
-	if (array && size && cmp)
-		while (x < size)
-		{
-			if (cmp(array[x]))
-				return (x);
-			x++;
-		}
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
+
 	return (-1);
 }
