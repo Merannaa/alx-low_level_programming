edd133e35d3c004a7c8bffbe3ba72891f24da566
meran <meran.ahmed.tr@gmail.com>
2023-05-26 02:18:45 -0700

    0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,10 +5,10 @@
 /**
  * sum_them_all - sum of all its parameters
  * @n: number arg
- * @...: integer to sum
  * Return: int sum
  */
-int sum_them_all(const unsigned int n, ...);
+
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
 	int sum = 0;
