f3057dbc3d39ed26fe98a754ca87b95b9cd75cb5
meran <meran.ahmed.tr@gmail.com>
2023-05-26 03:40:10 -0700

    Task.3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,23 +6,34 @@
  * @argv: argument
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv[])
 {
-	int(*op_func)(int, int), a, b;
-	
-	if(argc != 4)
-		printf("error\n"), exit(98);
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("error\n"), exit(99);
-	
-	if(!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("error\n"), exit(100);
-
-	printf("%d\n", op_func(a, b));
+	printf("%d\n", operation(a, b));
 	return (0);
 }
