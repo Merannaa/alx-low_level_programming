61ae4ed6aa5fbc34258ec9b478f6c2fa4bfb1354
meran <meran.ahmed.tr@gmail.com>
2023-05-26 02:09:12 -0700

    0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,16 @@ int sum_them_all(const unsigned int n, ...);
 	va_list valist;
 	int sum = 0;
 	unsigned int x;
+	
+	if (n == 0)
+		return (0);
 
 	va_start(valist, n);
 
 	for (x = 0; x < n; x++)
-	{
+
 		sum += va_arg(valist, int);
-	}
+
 
 	va_end(valist);
 
