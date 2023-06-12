1e5f746c896444f33b4e7189ec638aad9e6581da
meran <meran.ahmed.tr@gmail.com>
2023-06-12 16:32:28 -0700

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,16 +10,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int mr;
+	int m;
 	int mletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	mr = open(filename, O_WRONLY | O_APPEND);
+	m = open(filename, O_WRONLY | O_APPEND);
 
-	if (mr == -1)
+	if (m == -1)
 		return (-1);
 
 	if (text_content)
@@ -27,13 +27,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (mletters = 0; text_content[mletters]; mletters++)
 			;
 
-		rwr = write(mr, text_content, mletters);
+		rwr = write(m, text_content, mletters);
 
 		if (rwr == -1)
 			return (-1);
 	}
 
-	close(mr);
+	close(m);
 
 	return (1);
 }
